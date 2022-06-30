f83b9cfe35b80467e9eefc256ef80b3db0e8cfde
Khodja Moussa <khodja.moussa@yahoo.com>
2022-06-30 18:19:00 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -37,9 +37,10 @@ createAdmin()
 app.use(cors())
 app.use(express.json())
 app.use(accountManagementRouter)
-app.use(adminRouter)
 app.use(lesionRouter)
 app.use(userRouter)
+// admin routes after user, because of conflict [admin delete users/:id] and [user delete users/me]
+app.use(adminRouter)
 app.use(messageRouter)
 app.use((req, res, next) => {
     res.status(404).send({error: "Not Found!"})
--- src/routers/admin.js
+++ src/routers/admin.js
@@ -17,6 +17,19 @@ router.get('/users', auth, adminAuthorization, async (req, res) => {
     }
 })
 
+// this route has a conflict with the other route users/me 
+router.delete('/users/:id', auth, adminAuthorization, async (req, res) => {
+    try {
+        // const task = await Task.findByIdAndDelete(req.params.id)
+        const user = await User.findOneAndDelete({_id: req.params.id})
+        if(!user)
+            return res.status(404).send('No user with that id')
+        res.send(task)
+    }catch(e) {
+        res.status(500).send(e)
+    }
+})
+
 
 
 // TODO: make it a general update route with patch method but only allow verifiedDoctor to be updated 
@@ -25,7 +38,7 @@ router.post('/users/:id/verifyDoctor',auth, adminAuthorization, async (req, res)
         const user = await User.findById(req.params.id)
         if(!user)
             throw new Error()
-        user.verifiedDoctor = true
+        user.verifiedDoctor = !user.verifiedDoctor
         await user.save()
         res.send()
     }catch(e) {
